dae4ac88d54bf5364348ffe8f4eafe60a0ce5ccc
chrislzm <me@chrisleung.com>
2017-09-15 15:59:36 -0700

    style: Fix indentation

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Book from './Book'
 
 const BookShelf = (props) => (
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{props.title}</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              { props.books.map(book => (
-                <Book book={book} key={book.id} />
-              ))}
-            </ol>
-          </div>
-        </div>
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{props.title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        { props.books.map(book => (
+          <Book book={book} key={book.id} />
+        ))}
+      </ol>
+    </div>
+  </div>
 );
 
 export default BookShelf
