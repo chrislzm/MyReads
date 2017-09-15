64f56cec61dedd56353cb7ceb0fe32ce808f3ec8
chrislzm <me@chrisleung.com>
2017-09-14 20:54:49 -0700

    feat: Now using BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookShelf from './BookShelf'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -48,33 +49,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      { this.state.books && this.state.books.filter(book => book.shelf === "currentlyReading").map(book => (
-                      <li key={book.id}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{ book.title }</div>
-                          <div className="book-authors">{ book.authors[0] }</div>
-                        </div>
-                      </li>
-                    ))}
-                    </ol>
-                  </div>
-                </div>
+                { this.state.books && (
+                <BookShelf books={this.state.books.filter(book => book.shelf === "currentlyReading")} title="Currently Reading" />
+              )}
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
