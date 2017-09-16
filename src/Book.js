dd3856e26eb3126b99aa2f13a540ccde3e6d2787
chrislzm <me@chrisleung.com>
2017-09-15 21:03:41 -0700

    style: Fixed indents

--- src/Book.js
+++ src/Book.js
@@ -2,54 +2,54 @@ import React from 'react'
 
 const shelves = [
   { //  This first shelf is a label for the select element, so it's disabled
-    id: "move",
-    name: "Move to...",
-    disabled: true
-  },
-  {
-    id: "currentlyReading",
-    name: "Currently Reading",
-    disabled: false
-  },
-  {
-    id: "wantToRead",
-    name: "Want to Read",
-    disabled: false
-  },
-  {
-    id: "read",
-    name: "Read",
-    disabled: false
-  },
-  {
-    id: "none",
-    name: "None",
-    disabled: false
-  },
+  id: "move",
+  name: "Move to...",
+  disabled: true
+},
+{
+  id: "currentlyReading",
+  name: "Currently Reading",
+  disabled: false
+},
+{
+  id: "wantToRead",
+  name: "Want to Read",
+  disabled: false
+},
+{
+  id: "read",
+  name: "Read",
+  disabled: false
+},
+{
+  id: "none",
+  name: "None",
+  disabled: false
+},
 ]
 
 const Book = (props) => {
   let shelf
   shelf = props.book.shelf === undefined ? "none" : props.book.shelf
   return(
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.book.imageLinks ? `url(${props.book.imageLinks.thumbnail})` : ''}}></div>
-        <div className="book-shelf-changer">
-          <select value={ shelf } onChange={ (event) => props.handleChange(props.book,event.target.value)}>
-            {
-              shelves.map(shelf => (
-                <option value={shelf.id} disabled={shelf.disabled} key={shelf.id}>{shelf.name}</option>
-              ))
-            }
-          </select>
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.book.imageLinks ? `url(${props.book.imageLinks.thumbnail})` : ''}}></div>
+          <div className="book-shelf-changer">
+            <select value={ shelf } onChange={ (event) => props.handleChange(props.book,event.target.value)}>
+              {
+                shelves.map(shelf => (
+                  <option value={shelf.id} disabled={shelf.disabled} key={shelf.id}>{shelf.name}</option>
+                ))
+              }
+            </select>
+          </div>
         </div>
+        <div className="book-title">{ props.book.title }</div>
+        <div className="book-authors">{ props.book.authors && (props.book.authors[0]) }</div>
       </div>
-      <div className="book-title">{ props.book.title }</div>
-      <div className="book-authors">{ props.book.authors && (props.book.authors[0]) }</div>
-    </div>
-  </li>
-)};
+    </li>
+  )};
 
-export default Book
+  export default Book
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,18 +4,18 @@ import Book from './Book'
 const BookShelf = (props) => (
   <div>
     { props.books && props.books.length > 0 && (
-  <div className="bookshelf">
-    <h2 className="bookshelf-title">{props.title}</h2>
-    <div className="bookshelf-books">
-      <ol className="books-grid">
-        { props.books.map(book => (
-          <Book handleChange={props.handleChange} book={book} key={book.id} />
-        ))}
-      </ol>
-    </div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            { props.books.map(book => (
+              <Book handleChange={props.handleChange} book={book} key={book.id} />
+            ))}
+          </ol>
+        </div>
+      </div>
+    )}
   </div>
-  )}
-</div>
 );
 
 export default BookShelf
