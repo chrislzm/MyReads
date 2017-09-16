8e73755f134ff90005564474635eca7c06f2fbba
chrislzm <me@chrisleung.com>
2017-09-15 19:41:02 -0700

    fix: Added input validation to make sure we have books

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Book from './Book'
 
 const BookShelf = (props) => (
+  <div>
+    { props.books && props.books.length > 0 && (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{props.title}</h2>
     <div className="bookshelf-books">
@@ -12,6 +14,8 @@ const BookShelf = (props) => (
       </ol>
     </div>
   </div>
+  )}
+</div>
 );
 
 export default BookShelf
