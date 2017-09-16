e5a8795ff241a05a3bf2690f76442a7c184e56f7
chrislzm <me@chrisleung.com>
2017-09-16 15:16:26 -0700

    style: Reformatted for readability

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -5,23 +5,36 @@ import BookShelf from './BookShelf'
 const ListBooks = (props) => {
   const {books,handleChange} = props
   return(
-  <div className="list-books">
-    <div className="list-books-title">
-      <h1>MyReads</h1>
-    </div>
-    <div className="list-books-content">
-      <div>
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
         <div>
-          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "currentlyReading")} title="Currently Reading" />
-          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "wantToRead")} title="Want to Read" />
-          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "read")} title="Read" />
+          <div>
+            <BookShelf
+              title="Currently Reading"
+              books={books.filter(book => book.shelf === "currentlyReading")}
+              handleChange={handleChange}
+            />
+            <BookShelf
+              title="Want to Read"
+              books={books.filter(book => book.shelf === "wantToRead")}
+              handleChange={handleChange}
+            />
+            <BookShelf
+              title="Read"
+              books={books.filter(book => book.shelf === "read")}
+              handleChange={handleChange}
+            />
+          </div>
         </div>
       </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
     </div>
-    <div className="open-search">
-      <Link to="/search">Add a book</Link>
-    </div>
-  </div>
-)};
+  )
+};
 
 export default ListBooks
