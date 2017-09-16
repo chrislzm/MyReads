bee470ec913668a2ebd8b42d2bbe831528008313
chrislzm <me@chrisleung.com>
2017-09-16 15:13:43 -0700

    refactor: Using destructuring in place of long variable names

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
-const ListBooks = (props) => (
+const ListBooks = (props) => {
+  const {books,handleChange} = props
+  return(
   <div className="list-books">
     <div className="list-books-title">
       <h1>MyReads</h1>
@@ -10,9 +12,9 @@ const ListBooks = (props) => (
     <div className="list-books-content">
       <div>
         <div>
-          <BookShelf handleChange={props.handleChange} books={props.books.filter(book => book.shelf === "currentlyReading")} title="Currently Reading" />
-          <BookShelf handleChange={props.handleChange} books={props.books.filter(book => book.shelf === "wantToRead")} title="Want to Read" />
-          <BookShelf handleChange={props.handleChange} books={props.books.filter(book => book.shelf === "read")} title="Read" />
+          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "currentlyReading")} title="Currently Reading" />
+          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "wantToRead")} title="Want to Read" />
+          <BookShelf handleChange={handleChange} books={books.filter(book => book.shelf === "read")} title="Read" />
         </div>
       </div>
     </div>
@@ -20,6 +22,6 @@ const ListBooks = (props) => (
       <Link to="/search">Add a book</Link>
     </div>
   </div>
-);
+)};
 
 export default ListBooks
