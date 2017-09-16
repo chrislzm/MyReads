637153836114d166fd82061ce94739d537642835
chrislzm <me@chrisleung.com>
2017-09-15 19:03:45 -0700

    refactor: Moved search books into its own component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -30,30 +30,11 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        )}/>
         <Route exact path="/" render={() =>(
           <ListBooks handleChange={this.updateBook} books={this.state.books}/>
+        )}/>
+        <Route path="/search" render={() => (
+          <SearchBooks handleChange={this.updateBook} books={this.state.books}/>
         )}/>
       </div>
     )
