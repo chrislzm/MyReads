f55238ec4e3e9ba9cd91bd67cbabe16080f8bf9d
chrislzm <me@chrisleung.com>
2017-09-15 18:05:48 -0700

    feat: Dynamically generating select element options + selecting default

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,45 @@
 import React from 'react'
 
+const shelves = [
+  { //  This first shelf is a label for the select element, so it's disabled
+    id: "move",
+    name: "Move to...",
+    disabled: true
+  },
+  {
+    id: "currentlyReading",
+    name: "Currently Reading",
+    disabled: false
+  },
+  {
+    id: "wantToRead",
+    name: "Want to Read",
+    disabled: false
+  },
+  {
+    id: "read",
+    name: "Read",
+    disabled: false
+  },
+  {
+    id: "none",
+    name: "None",
+    disabled: false
+  },
+]
+
 const Book = (props) => (
   <li>
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
         <div className="book-shelf-changer">
-          <select onChange={ (event) => props.handleChange(props.book,event.target.value)}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
+          <select value={props.book.shelf} onChange={ (event) => props.handleChange(props.book,event.target.value)}>
+            {
+              shelves.map(shelf => (
+                <option value={shelf.id} disabled={shelf.disabled} key={shelf.id}>{shelf.name}</option>
+              ))
+            }
           </select>
         </div>
       </div>
