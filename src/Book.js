2fba9abaa694237d31a5aaf64c30ec9fe58b2f70
chrislzm <me@chrisleung.com>
2017-09-16 17:08:02 -0700

    feat: Now displaying all authors of a book refactor: Update variables for readability

--- src/Book.js
+++ src/Book.js
@@ -28,26 +28,53 @@ const shelves = [
 },
 ]
 
+const createAuthorList = (authors) => {
+  if(!authors) return ''
+  let authorList
+  for(const author of authors) {
+    console.log(author)
+    if(!authorList) {
+      authorList = author
+    } else {
+      authorList = `${authorList}, ${author}`
+    }
+  }
+  return authorList
+}
+
 const Book = (props) => {
   const {book,handleChange} = props
   let shelf = book.shelf ? book.shelf : "none"
-  return(
+  let authors = createAuthorList(book.authors)
+  let thumbnailUrl = book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "none"
+  return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.book.imageLinks ? `url(${props.book.imageLinks.thumbnail})` : ''}}></div>
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: thumbnailUrl
+            }}>
+          </div>
           <div className="book-shelf-changer">
             <select
               value={ shelf }
               onChange={ (event) => handleChange(book,event.target.value)}>
-              { shelves.map(shelf => (
-                <option value={shelf.id} disabled={shelf.disabled} key={shelf.id}>{shelf.name}</option>
+              {shelves.map(shelf => (
+                <option
+                  value={shelf.id}
+                  disabled={shelf.disabled}
+                  key={shelf.id}>{shelf.name}>
+                </option>
               ))}
             </select>
           </div>
         </div>
-        <div className="book-title">{ props.book.title }</div>
-        <div className="book-authors">{ props.book.authors && (props.book.authors[0]) }</div>
+        <div className="book-title">{ book.title }</div>
+        <div className="book-authors">{ authors }</div>
       </div>
     </li>
   )};
