f7205ca3c5fcdd2dd2d0919855c44d22657ddbc4
chrislzm <me@chrisleung.com>
2017-09-18 19:46:37 -0700

    feat: Made bookshelves a constant. Can now easily add/remove bookshelves

---
+++ src/Constants.js
@@ -0,0 +1,28 @@
+export const SHELVES =
+  [
+    { // First shelf acts as a label for the other options so it's disabled
+    id: "move",
+    name: "Move to...",
+    disabled: true
+  },
+  {
+    id: "currentlyReading",
+    name: "Currently Reading",
+    disabled: false
+  },
+  {
+    id: "wantToRead",
+    name: "Want to Read",
+    disabled: false
+  },
+  {
+    id: "read",
+    name: "Read",
+    disabled: false
+  },
+  {
+    id: "none",
+    name: "None",
+    disabled: false
+  }
+]
